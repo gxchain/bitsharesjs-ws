e260521f71c422f9f8dddaceb6582fc2139c73c6
lanhaoxiang <lanhaoxiang@qq.com>
2018-12-14 14:42:59 +0800

    chore: optimize connection manager

--- test/manager.js
+++ test/manager.js
@@ -1,6 +1,6 @@
 import {Manager} from '../lib';
 
-let manager = new Manager ({url: 'wss://node5.gxb.io', urls: ['wss://node5.gxb.io'], autoFallback: true});
+let manager = new Manager ({url: 'wss://node5.gxb.io', urls: ['wss://node5.gxb.io','wss://node1.gxb.io'], autoFallback: true});
 
 manager.connectWithFallback (true).then (() => {
     setTimeout (() => {
